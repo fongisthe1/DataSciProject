92c63d6c472c0ae28bd40f4a76ef6d95a613d93f
fongisthe1 <hawsatitamnantat@gmail.com>
2024-12-07 17:28:09 +0700

    Fixed Arxiv format and added in visualization

--- WebScraping/cleaning3.py
+++ WebScraping/cleaning3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("./Database-files/final3_arxiv_articles.csv")
+df = pd.read_csv("./final3_arxiv_articles.csv")
 
 mapper1 = {
     'adap-org':'Adaptation and Self-Organizing Systems',
@@ -28,9 +28,5 @@ df.loc[missing_mask, 'Subject Areas'] = df.loc[missing_mask, 'cat_id'].map(mappe
 temp = df[df['Subject Areas'].isnull()].groupby('cat_id').count()
 print(temp)
 #Remove Time of day of publication
-df['Publication Date'] = df['Publication Date'].apply(lambda x: x.split('T')[0])
-print(df.head)
-
-#Save to the file
-df.to_csv('./Database-files/final4_arxiv_articles.csv',index=False)
+df.to_csv('./final12_arxiv_articles.csv',index=False)
 print("Successfully Saved")
